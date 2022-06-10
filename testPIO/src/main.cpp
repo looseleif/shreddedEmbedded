012bf1c454bf14c33878c7cb960569832af5356f
Chase <chase.anderson.000@gmail.com>
2022-06-10 02:23:19 -0500

    Using ATMEGA64A core

---
+++ testPIO/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  digitalWrite(4,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
